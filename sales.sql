90a123d0b0b9bd49ec7976262f140e4101effb4c
gagebauer51 <95508366+gagebauer51@users.noreply.github.com>
2023-04-24 19:48:33 -0400

    Update sales.sql

--- sales.sql
+++ sales.sql
@@ -1,1 +1,8 @@
 /* List all employees in the Sales department, including their employee number, last name, first name, and department name. */
+/* List all employees in the Sales department, including their employee number, last name, first name, and department name. */
+SELECT D.department_name, D.dept_no, DE.emp_no
+FROM Department as D 
+INNER JOIN Dept_Emp as DE 
+ON D.dept_no = DE.dept_no
+INNER JOIN Employees as E
+ON DE.emp_no = E.emp_no;
