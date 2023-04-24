c4551a79f2da6cfac1b00348acb4c9da3ddb60de
gagebauer51 <95508366+gagebauer51@users.noreply.github.com>
2023-04-24 19:59:34 -0400

    Update emp_dept.sql
    I am starting to do manual commits because I have spent many hours trying to get my visual code to commit correctly, and it's been a mess.

--- emp_dept.sql
+++ emp_dept.sql
@@ -1,1 +1,5 @@
 /* List the department of each employee with the following information: employee number, last name, first name, and department name. */
+Select e.emp_no, e.last_name, e.first_name, d.dept_name
+from Employees e
+join Dept_Emp de ON e.emp_no = de.emp_no
+join Departments d on de.dept_no = d.dept_no;
