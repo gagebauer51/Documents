c79509ac9738cb5576f2c56ff1ad2a63844b453c
gagebauer51 <95508366+gagebauer51@users.noreply.github.com>
2023-04-24 19:50:53 -0400

    Update doh1986.sql

--- doh1986.sql
+++ doh1986.sql
@@ -1,4 +1,4 @@
 /* List employees who were hired in 1986. */
-SELECT *, strftime('Y%', hire_date) AS hire_year
+SELECT *, strftime('%Y', hire_date) AS hire_year
 FROM Employees
-WHERE strftime('Y%', hire_date) = '1986';
+WHERE strftime('%Y', hire_date) = '1986';
