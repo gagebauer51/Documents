c1d864c3a116d9dadbcf520d4ce93febe510f153
gagebauer51 <95508366+gagebauer51@users.noreply.github.com>
2023-04-24 20:28:48 -0400

    Update managers.sql

--- managers.sql
+++ managers.sql
@@ -1,1 +1,5 @@
 /* List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates. */
+Select d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name, dm.from_date, dm.to_date
+From Departments d
+Join Dept_Manager dm ON d.dept_no = dm.dept_no
+Join Employees e ON dm.emp_no = e.emp_no;
